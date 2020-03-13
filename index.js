459783201c8d461278accbb0cb9fcd403300d044
Alexander Karpov <kukuruku@yandex-team.ru>
2020-03-14 00:54:33 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const { spawn } = require('child_process');
+
+module.exports.handler = function handler(event) {
+    const text = event.queryStringParameters.text;
+
+    if (!text) {
+        return {
+            statusCode: '400 BadRequest',
+            body: 'Query parameter "text" was not provided.'
+        }
+    }
+
+    let mystem;
+
+    try {
+        mystem = spawn('./mystem', ['--format=json', '--weight', '-i']);
+    } catch (error) {
+        return {
+            statusCode: '500 Internal Server Error',
+            body: error.toString()
+        }
+    }
+
+    const promise = new Promise((resolve, reject) => {
+        let output;
+
+        mystem.stdout.on('data', data => {
+            output = data;
+        });
+
+        mystem.stderr.on('data', error => {
+            reject({
+                statusCode: '500 Internal Server Error',
+                body: error.toString()
+            });
+        });
+
+        mystem.stdout.on('close', () => {
+            if (!output) {
+                resolve({
+                    statusCode: '200 OK',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: '[]'
+                });
+
+                return;
+            }
+
+            resolve({
+                statusCode: '200 OK',
+                headers: { 'Content-Type': 'application/json' },
+                body: output.toString()
+            });
+        });
+    });
+
+    const cyrillic = removeNonCyrillic(text);
+
+    mystem.stdin.write(`${cyrillic}\n`);
+    mystem.stdin.end();
+
+    return promise;
+}
+
+/**
+ * Оставляет в тексте только кирилические симводы и пробелы
+ * Удаляет из текст то, что мы явно не можем обработать
+ * @param message {String}
+ * @returns {String}
+ */
+function removeNonCyrillic(message) {
+    return message.replace(/[^а-яА-ЯёЁ ]+/g, '');
+}
